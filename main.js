767512fd261848b6720418be31490c8c9a0154d3
kei3570 <67131453+kei3570@users.noreply.github.com>
2024-02-29 12:09:15 +0900

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 //グローバルスコープ＿関数外に置くことで回数の累計が表示されるように設定できる逆に、関数内に入れると呼び出されるたびに0に回帰するので関数の外に置く
-let Result_count = 0; 
+var Result_wincount = parseInt(localStorage.getItem('wins')) || 0;  
+var Result_drawcount = parseInt(localStorage.getItem('draw')) || 0;  
+var Result_losecount = parseInt(localStorage.getItem('lose')) || 0;  
+
 
 function R_Click(p_janken_r){
    //プレイヤー側のじゃんけん手札
@@ -30,13 +33,18 @@ function R_Click(p_janken_r){
   // }
   if (janken_r === p_janken_r){
     Result_end = "あいこ";
+    Result_drawcount += 1; 
+    localStorage.setItem('draw',Result_drawcount);
 } else if ((p_janken_r === 0 && janken_r === 1) ||
            (p_janken_r === 1 && janken_r === 2) ||
            (p_janken_r === 2 && janken_r === 0)){
     Result_end = "勝ち";
-    Result_count += 1; // 勝った場合に勝ち回数を増やす
+    Result_wincount += 1; // 勝った場合に勝ち回数を増やす
+    localStorage.setItem('wins',Result_wincount);
 } else {
     Result_end = "負け";
+    Result_losecount += 1; 
+    localStorage.setItem('lose',Result_losecount);
 }
 
 
@@ -47,8 +55,26 @@ function R_Click(p_janken_r){
   document.getElementById("Rejan1").innerHTML = "自分は" + p_janken[p_janken_r] +"出しました";
   document.getElementById("Rejan2").innerHTML = "相手は" + janken[janken_r] +"出しました" ;
   document.getElementById("Rejan3").innerHTML = Result_end;
-  document.getElementById("Rejan4").innerHTML = "勝ち回数: " + Result_count;
+  document.getElementById("wincount").innerHTML = Result_wincount;
+  document.getElementById("drawcount").innerHTML = Result_drawcount;
+  document.getElementById("losecount").innerHTML = Result_losecount;
   document.getElementById("jankenpon2").src="jan" + janken_r + ".png";
 
+}
+
+function Reset_Click(){
+  localStorage.setItem('win',0)
+  localStorage.setItem('draw',0)
+  localStorage.setItem('lose',0)
+  Result_wincount = 0;
+  Result_drawcount = 0;
+  Result_losecount = 0;
+
+  document.getElementById("Rejan1").innerHTML = "";
+  document.getElementById("Rejan2").innerHTML = "";
+  document.getElementById("Rejan3").innerHTML = "リセットしました";
+  document.getElementById("wincount").innerHTML = 0;
+  document.getElementById("drawcount").innerHTML = 0;
+  document.getElementById("losecount").innerHTML = 0;
 }
 
